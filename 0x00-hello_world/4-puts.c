67623d7655fde8d556334bb7ff9e7326e351b143
Frank4140 <frankbuduka@yahoo.com>
2023-08-12 04:24:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,8 @@
  *
  * return - 0 (successful)
  */
-int main() 
-
+int main(void) 
 {
-
-	puts("Programming is like building a multilingual puzzle");
-	return o;
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
