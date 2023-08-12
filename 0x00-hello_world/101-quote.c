831e50bd956e354bb4a9cb83d5638cfd9df3a9cb
Frank4140 <frankbuduka@yahoo.com>
2023-08-12 09:13:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - a C program that prints without using printff and puts
+ * main - Printing without using printf() or puts()
  *
- * Return: 1 successful
+ * Return: Always 1 (successful)
  */
+
 int main(void)
 {
+
 	write(2,
-		"and that piece of art is useful\" - Dora Korpar,
-			2015-10-19\n", 59);
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
 	return (1);
+
 }
