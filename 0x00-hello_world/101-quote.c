b7d6d4f9872b8f163ca549d19fbea808e6ec554d
Frank4140 <frankbuduka@yahoo.com>
2023-08-12 08:58:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints without using printff and puts
+ * main - a C program that prints without using printff and puts
  *
  * Return: 1 successful
  */
