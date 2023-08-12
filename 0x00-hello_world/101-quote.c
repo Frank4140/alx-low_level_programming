251ae42febc36c272c81dd77128335773c074559
Frank4140 <frankbuduka@yahoo.com>
2023-08-12 08:32:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that prints without using printff and puts
+ *
+ * Return: 1 successful
+ */
+int main(void)
+{
+	fprint(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
