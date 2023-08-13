79d8df944cd10e3b04652af43a4610306f8bc711
Frank4140 <frankbuduka@yahoo.com>
2023-08-13 14:41:30 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,22 +9,17 @@
 int main(void)
 
 {
-
 	int i;
 
-
 	for (i = 0; i < 10; i++)
-
 	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
 
-		putchar(i);
-		putchar (',');
-		putchar (' ');
-		
 	}
 
-	putchar ('\n');
-
+	putchar('\n');
 	return (0);
 
 }
